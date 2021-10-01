daee3a4c9773eede87518fb44d1a2ea0174bce81
drway <78285587+drway@users.noreply.github.com>
2021-10-01 17:38:05 +0800

    Format the code, added information into readme.md
    Format the code.
    add information into readme.md

--- Fast_IO.cpp
+++ Fast_IO.cpp
@@ -1,10 +1,31 @@
-#include<bits/stdc++.h>
-using namespace std;
+// Fast IO for Competitive Programming 
+// Codes by drway
+// Copyright (C) drway
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// Mozilla Public License 2.0 for more details.
+
+// You should have received a copy of the Mozilla Public License 2.0
+// along with this program;
+// If not, see <https://www.mozilla.org/en-US/MPL/>.
+
+// HEADERS SHOULD BE INCLUDED HERE
+//To run this code, you must include cstdio/stdio.h
+
+#include<cstdio>
+
+// the size of the buffer, which is believed to be the fastest when the size is 2^15
+#define bufSIZE 1<<15
+
+// definations for reading in
 #ifdef ONLINE_JUDGE
-#define getchar()(p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++)
-char buf[1<<21],*p1=buf,*p2=buf;
+#define getchar()(in_p1==in_p2&&(in_p2=(in_p1=in_buf)+std::fread(in_buf,1,bufSIZE,stdin),in_p1==in_p2)?EOF:*in_p1++)
+char in_buf[bufSIZE],*in_p1=buf,*in_p2=buf;
 #endif
-#define int long long
+
+// functions to read in
 template<typename T>inline void redi(T& ret) {
     ret=0;T f=1;char ch=getchar();
     while (ch<'0'||ch>'9') {if (ch=='-') f=-f;ch=getchar();}
@@ -15,18 +36,22 @@ template <typename T,typename... Args> inline void redi(T& t, Args&... args)
 {
     redi(t);redi(args...);
 }
-char buffer[1<<21];
-int p11=-1;
-const int p22=(1<<21)-1;
+
+// definations for printing out
+char out_buf[bufSIZE];
+int out_p1=-1;
+const int out_p2=(bufSIZE)-1;
+
+// functions to print out
 inline void flush(){
-  fwrite(buffer,1,p11+1,stdout),p11=-1;
+  std::fwrite(out_buf,1,out_p1+1,stdout),out_p1=-1;
 }
 inline void putc(const char &x){
-  if (p11==p22)flush();
-  buffer[++p11]=x;
+  if (out_p1==out_p2)flush();
+  out_buf[++out_p1]=x;
 }
 template<typename T>inline void wrtn(T x){
-  static char buf[15];
+  static char buf[20];
   static T len=-1;
   if (x>=0){
     do{
@@ -45,7 +70,7 @@ template<typename T>inline void wrtn(T x){
 template<typename T>inline void wrti(T x){
 	wrtn(x),putc('\n');
 }
-signed main(){
-	
-	return 0;
-}
+
+//start your program here
+
+
--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 Cpp Fast IO Templates by drway.
 
 The code is under MPL 2.0 Licence.
+
+This template is used for competitive programming, for a faster read in and print out speed.
+
+Need C++ 11 Support.
+
+## Functions
+
+* ```redi()``` is used to read in integers. You can use it like ```redi(a,b,c,...)``` . It uses variadic templates so your compiler must support C++ 11 or later version of C++.
+
+* ```getchar()``` is a macro defined to get a character using the function ```fread()``` . (It is not the function defined in cstdio/stdio.h !)
+
+* ```wrtn()``` is used to write an integer. You can use it like ```wrtn(a)``` .
+
+* ```wrti()``` is used to write an integer and feed the line. You can use it like ```wrti(a)``` .
+
+# Functions need to be added
+
+* read in a string
+* print a string
